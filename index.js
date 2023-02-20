3eda7620f290e3cc502d9c08564e0a9d116a94d1
bachiito <riveraabraham332@gmail.com>
2023-02-19 20:28:25 -0400

    set clock hands rotation according to local time

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+const secondsHand = document.querySelector(".sec-hand");
+const minutesHand = document.querySelector(".min-hand");
+const hoursHand = document.querySelector(".hour-hand");
+
 window.addEventListener("resize", setClockHeight);
 
 setClockHeight();
+setInterval(setTime, 1000);
+
+function setTime() {
+  const now = new Date();
+
+  if (now.getSeconds() === 0) {
+  }
+
+  secondsHand.style.rotate = `${now.getSeconds() * 6 + 90}deg`;
+  minutesHand.style.rotate = `${now.getMinutes() * 6 + 90}deg`;
+  hoursHand.style.rotate = `${now.getHours() * 30 + 90}deg`;
+}
 
 function setClockHeight() {
   const clock = document.querySelector(".clock");
